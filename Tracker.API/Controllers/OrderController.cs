e51c956f20224ca44b1e926eabc343d82674f0dd
NivedhaLakshmanan <Nivedha Lakshmanan@DESKTOP-8K9HAFS>
2021-10-29 00:57:36 +0530

    fix: Order controller

---
+++ Tracker.API/Controllers/OrderController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tracker.Models.ViewModel;
+using Tracker.Services.Interface;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService, ILogger<OrderController> logger)
+        {
+            _orderService = orderService;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                var result = await _orderService.GetAllAsync();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            try
+            {
+                var result = await _orderService.GetByIdAsync(id);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+        }
+
+        [HttpPost("addOrUpdate")]
+        public async Task<IActionResult> AddOrUpdateAsync([FromBody] OrderViewModel order)
+        {
+            try
+            {
+                var result = await _orderService.AddOrUpdateAsync(order);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+        }
+    }
+}
